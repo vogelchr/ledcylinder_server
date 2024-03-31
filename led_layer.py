76cbc73d065a46948007276027d50092173aa58c
Christian Vogel <vogelchr@vogel.cx>
2024-03-31 17:23:54 +0200

    support animations

--- led_layer.py
+++ led_layer.py
@@ -3,6 +3,7 @@ import PIL.Image
 from abc import abstractmethod, ABC
 from logging import debug, info, warning, fatal, exception
 import numpy as np
+from typing import List
 
 
 class LED_Layer(ABC):
@@ -56,3 +57,56 @@ class LED_Image(LED_Layer):
         pass
 
 
+class LED_Anim(LED_Layer):
+    img_arr: List[PIL.Image]
+    img_ix: int
+
+    def __init__(self, width: int, height: int, img_arr: List[PIL.Image]):
+        self.width = width
+        self.height = height
+        self.img_arr = img_arr
+        self.img_ix = 0
+
+    @classmethod
+    def from_file(cls, fn: Path, limit_brightness: int):
+        frames = []
+        shape = None
+
+        info(f'Loading animation from {fn}...')
+        with fn.open() as f:
+            for line in f:
+                if (ix := line.find('#')) != -1:
+                    line = line[:ix]
+                line = line.strip()
+                if not line:
+                    continue
+
+                img_fn = fn.parent / fn.stem / line
+                info(f' frame {img_fn}.')
+
+                ndarr = np.array(PIL.Image.open(img_fn))
+                if shape is None:
+                    shape = ndarr.shape
+                assert np.array_equal(shape, ndarr.shape)
+                frames.append(ndarr)
+
+        frames = np.stack(frames, 0)
+
+        vmax = np.amax(frames)
+        if vmax > limit_brightness:
+            print(f'{fn}: too bright {vmax}, limiting to {limit_brightness}...')
+            frames = np.round(frames * (limit_brightness / vmax)).astype(np.uint8)
+
+        img_arr = []
+        for k in range(frames.shape[0]):
+            img_arr.append(PIL.Image.fromarray(frames[k]))
+        info('Animation with {len(img_arr)} frames of size {shape[1]} x {shape[0]}')
+        return cls(shape[1], shape[0], img_arr)
+
+    def tick(self):
+        self.img_ix += 1
+        if self.img_ix >= len(self.img_arr) :
+            self.img_ix = 0
+
+    def get(self) :
+        return self.img_arr[self.img_ix]
